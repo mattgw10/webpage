1cbba83eb5145e7a15c41e25ee373cf61d487574
mattgw10 <mgw10@wildcats.unh.edu>
2020-10-30 15:22:47 -0400

    Updates

---
+++ 252f366e-743d09cdd64610203fa2.js
@@ -0,0 +1,2 @@
+(window.webpackJsonp=window.webpackJsonp||[]).push([[4],{NIcq:function(c,t,n){"use strict";n.d(t,"a",(function(){return h}));var a=n("Lnxd");function h(c){return Object(a.a)({tag:"svg",attr:{viewBox:"0 0 24 24"},child:[{tag:"path",attr:{d:"M20 6h-4V4c0-1.11-.89-2-2-2h-4c-1.11 0-2 .89-2 2v2H4c-1.11 0-1.99.89-1.99 2L2 19c0 1.11.89 2 2 2h16c1.11 0 2-.89 2-2V8c0-1.11-.89-2-2-2zm-6 0h-4V4h4v2z"}}]})(c)}}}]);
+//# sourceMappingURL=252f366e-743d09cdd64610203fa2.js.map
--- README.md
+++
@@ -1,3 +0,0 @@
-# Matt Westbrook's Webpage
-
-This is my published web page, for the source files see the source branch of this repository.
